60d5d43db0ece18cb3c3641244121955d1098247
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-05 23:12:03 +0600

    Added options to run multiple times

--- Collatz.py
+++ Collatz.py
@@ -1,18 +1,29 @@
-number=int(input("Enter a number: "))
-max_number,min_number,i,steps=number,number,1,0
-while number >=i:
-    if number%2==0 or number ==1:
-        number = number /2
+def main():
+    number=int(input("Enter a number: "))
+    max_number,min_number,i,steps=number,number,1,0
+    while number >=i:
+        if number%2==0 or number ==1:
+            number = number /2
+        else:
+            number = (number*3)+1
+        print(number)
+        if number > max_number:
+            max_number = number
+        if number < min_number:
+            min_number = number
+        steps+=1
+
+    print(f" Max Value: {max_number}")
+    print(f" Min Value: {min_number}")
+    print(f" Total Steps: {steps}\n")
+
+    print("[1] to try again")
+    print("[0] EXIT")
+    ch = input("")
+    if ch == "1":
+        main()
     else:
-        number = (number*3)+1
-    print(number)
-    if number > max_number:
-        max_number = number
-    if number < min_number:
-        min= number
-    steps+=1
+        print("###BYE###")
 
-
-print(f" Max Value: {max_number}")
-print(f" Min Value: {min_number}")
-print(f" Total Steps: {steps}\n")
+if __name__ == "__main__":
+    main()
